5398391c143857cc8fbe9572eac740e459b6559e
quanvda <quandang2102@gmail.com>
2022-10-28 22:26:37 +0700

    quêu

--- src/main/java/hanu/gdsc/infrastructure/publisher/SubmissionEventPublisherImpl.java
+++ src/main/java/hanu/gdsc/infrastructure/publisher/SubmissionEventPublisherImpl.java
@@ -3,7 +3,6 @@ package hanu.gdsc.infrastructure.publisher;
 import hanu.gdsc.domain.models.SubmissionEvent;
 import hanu.gdsc.domain.publisher.SubmissionEventPublisher;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Exchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +10,9 @@ import org.springframework.stereotype.Service;
 public class SubmissionEventPublisherImpl implements SubmissionEventPublisher {
     @Autowired
     private AmqpTemplate rabbitTemplate;
-    private static final String EXCHANGE = "DIRECTEXCHANGE";
     private static final String ROUTING_KEY = "COREPROBLEM_SUBMISSIONEVENT";
     @Override
     public void publish(SubmissionEvent submissionEvent) {
-        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, submissionEvent);
+        rabbitTemplate.convertAndSend(ROUTING_KEY, submissionEvent);
     }
 }
